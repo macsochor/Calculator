5f631f963b00560e6dd8163d974c221174fd6fcf
macsochor <MSMD@MSMDs-MacBook-Pro.local>
2017-10-11 19:52:27 -0400

    Added basic addition functionality

--- app/src/main/java/com/example/msmd/librarydemo/MainActivity.java
+++ app/src/main/java/com/example/msmd/librarydemo/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
@@ -15,6 +16,12 @@ import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
     @BindView(R.id.textView) TextView textView;
+    @BindView(R.id.button_plus) Button button_plus;
+    @BindView(R.id.button_equals) Button button_equals;
+    @BindView(R.id.button_clear) Button button_clear;
+
+
+    Integer firstNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,5 +38,38 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
         }
+
+        button_plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (textView.getText().length() > 0) {
+                    firstNumber = Integer.valueOf(textView.getText().toString());
+                    textView.setText("");
+                } else {
+                    Toast.makeText(MainActivity.this, "You must input a number", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        button_equals.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(firstNumber != null && textView.getText().length() > 0) {
+                    int secondNumber = Integer.valueOf(textView.getText().toString());
+                    String total = String.valueOf(secondNumber + firstNumber);
+                    textView.setText(total);
+                    firstNumber = null;
+                } else {
+                    Toast.makeText(MainActivity.this, "You must hit plus with a number first!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        button_clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                firstNumber = null;
+                textView.setText("");
+            }
+        });
     }
 }
